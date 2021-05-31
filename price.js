be892c6c0579d22912a85736b58dc51c97982e8d
cedricmesas <mesas.cedric@gmail.com>
2021-05-31 21:11:24 +0200

    Right Price Game

---
+++ price.js
@@ -0,0 +1,15 @@
+// Etape 1 - Sélectionner nos éléments
+let input = document.querySelector('#prix');
+let error = document.querySelector('small');
+let form = document.querySelector('#formulaire');
+
+// Etape 2 - Cacher l'erreur
+
+// Etape 3 - Générer un nombre aléatoire
+
+// Etape 4 - Vérifier que l'utilisateur donne bien un nombre
+
+// Etape 5 - Agir à l'envoi du formulaire
+
+// Etape 6 - Créer la fonction vérifier
+
